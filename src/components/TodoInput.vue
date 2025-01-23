e1d6ce9d23ac5a910b42389990ae7c208b2590cf
tony <tmtony98@gmail.com>
2025-01-23 17:16:19 +0530

    Revert "Merge branch 'computed' into defineEmit"
    This reverts commit de2e6d58e7b43c33bc58e0ab7de306592c1eee26, reversing
    changes made to 4b2cd034225b97402060726d597f01a9ee731063.

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import {onMounted, ref} from 'vue';
 import axios from "axios";
-import { computed ,watch } from 'vue';
+
 
 const inputvalue = ref<string>("")
 const todoList = ref<string[]>([])
 
-const publishedTODO= computed(() => {
- return inputvalue.value == "tony" ? "yes" : "no"
-})
-
+// const emit = defineEmits(['inFocus', 'submit'])
 
 const emit = defineEmits<{
   (event: 'submit', inputvalue: string): void
@@ -51,14 +48,12 @@ onMounted(() => {
             <div class="d-flex">
                 <input id="exampleInput" v-model="inputvalue" aria-describedby="emailHelp" class="form-control"
                        placeholder="Add new" type="text">
-                <button class="btn btn-primary mx-2  " @click.prevent="$emit('someEvent', inputvalue)">Add</button>
-                
+                <button class="btn btn-primary mx-2  " @click.prevent="emit('submit',inputvalue)">Add</button>
             </div>
             <!--            <ul v-for="list in todoList">-->
             <!--                <li>{{ list }}</li>-->
             <!--            </ul>-->
         </div>
-        <p class="">Entry is  tony - {{ publishedTODO }}</p>
     </form>
 
 </template>
